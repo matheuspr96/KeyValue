a21da70842eb6fc404f8bdc42ca76914ba46d63b
matheuspr96 <matheuspr96@hotmail.com>
2020-11-25 06:03:33 -0300

    initial commit

---
+++ src/main/java/com/sd/KeyValueUI/KeyValueUiApplication.java
@@ -0,0 +1,13 @@
+package com.sd.KeyValueUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyValueUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyValueUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sd/KeyValueUI/KeyValueUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.KeyValueUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
